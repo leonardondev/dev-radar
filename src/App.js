b74324a7ca82a6b78751f01cb5f05301fb2b23a4
LeonardoFuba <leonardo3.nascimento@gmail.com>
2020-01-16 18:52:32 -0300

    correcao className

--- src/App.js
+++ src/App.js
@@ -5,30 +5,30 @@ import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
-
 function App() {
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
         <form>
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
             <input name="github_username" id="github_username" required />
           </div>
 
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
             <input name="techs" id="techs" required />
           </div>
           
           <div className="input-group">
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
               <input name="latitude" id="latitude" required />
             </div>
 
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
               <input name="longitude" id="longitude" required />
             </div>
