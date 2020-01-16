056c36c04602000a548553a0ae2b60a35110570e
LeonardoFuba <leonardo3.nascimento@gmail.com>
2020-01-16 15:51:37 -0300

    main estruturado

--- src/App.js
+++ src/App.js
@@ -36,6 +36,56 @@ function App() {
         </form>
       </aside>
       <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4" alt="Diego Fernandes" />
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, Node.js</span>
+
+              </div>
+            </header>
+            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
+            <a href="https://github.com/diego3g">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4" alt="Diego Fernandes" />
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, Node.js</span>
+
+              </div>
+            </header>
+            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
+            <a href="https://github.com/diego3g">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4" alt="Diego Fernandes" />
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, Node.js</span>
+
+              </div>
+            </header>
+            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
+            <a href="https://github.com/diego3g">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4" alt="Diego Fernandes" />
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, Node.js</span>
+
+              </div>
+            </header>
+            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
+            <a href="https://github.com/diego3g">Acessar perfil no Github</a>
+          </li>
+        </ul>
       </main>
     </div>
 
