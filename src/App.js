1872e221007cbdeba8214a2b4db048859e6c7815
LeonardoFuba <leonardo3.nascimento@gmail.com>
2020-01-17 15:20:54 -0300

    move funcao de geolocalizacao e formulario para index do componente (nao funcional ainda)

--- src/App.js
+++ src/App.js
@@ -41,43 +41,7 @@ function App() {
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form onSubmit={handleAddDev} >
-          <div className="input-block">
-          <label htmlFor="github_username">Usuário do Github</label>
-          <input name="github_username" id="github_username" required
-              value={github_username}
-              onChange={e=> setGithubUsername(e.target.value)}
-          />
-          </div>
-
-          <div className="input-block">
-          <label htmlFor="techs">Tecnologias</label>
-          <input name="techs" id="techs" required
-              value={techs}
-              onChange={e=> setTechs(e.target.value)}
-          />
-          </div>
-          
-          <div className="input-group">
-          <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input type="number" name="latitude" id="latitude" required 
-                value={latitude}
-                onChange={e => setLatitude(e.target.value)}
-              />
-          </div>
-
-          <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input type="number" name="longitude" id="longitude" required 
-                value={longitude}
-                onChange={e => setLongitude(e.target.value)}
-              />
-          </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
@@ -85,7 +49,6 @@ function App() {
           {devs.map(dev => (
             <DevItem key={dev._id} dev={dev} />
           ))}
-          
         </ul>
       </main>
     </div>
