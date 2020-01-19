a28614675317336a5f6edfa2ea7c1e6db415a52e
LeonardoFuba <leonardo3.nascimento@gmail.com>
2020-01-19 12:04:32 -0300

    interface visual completa

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main( {navigation} ) {
    const [currentRegion, setCurrentRegion] = useState(null);
@@ -35,21 +36,35 @@ function Main( {navigation} ) {
    }
 
    return (
-      <MapView initialRegion={currentRegion} style={styles.map}>
-         <Marker coordinate= {{latitude: -21.1461433, longitude: -47.9964195}}>
-            <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/2254731?s=460&v=4' }} />
+      <>
+         <MapView initialRegion={currentRegion} style={styles.map}>
+            <Marker coordinate= {{latitude: -21.1461433, longitude: -47.9964195}}>
+               <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/2254731?s=460&v=4' }} />
 
-            <Callout onPress={()=>{
-               navigation.navigate('Profile',{ github_username:'diego3g' });
-            }} >
-               <View style={styles.callout}>
-                  <Text style={styles.devName} >Diego Fernandes</Text>
-                  <Text style={styles.devBio} >CTO na Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</Text>
-                  <Text style={styles.devTechs} >ReactJS, React Native, Node.js</Text>
-               </View>
-            </Callout>
-         </Marker>
-      </MapView>
+               <Callout onPress={()=>{
+                  navigation.navigate('Profile',{ github_username:'diego3g' });
+               }} >
+                  <View style={styles.callout}>
+                     <Text style={styles.devName} >Diego Fernandes</Text>
+                     <Text style={styles.devBio} >CTO na Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</Text>
+                     <Text style={styles.devTechs} >ReactJS, React Native, Node.js</Text>
+                  </View>
+               </Callout>
+            </Marker>
+         </MapView>
+         <View style={styles.serchForm}>
+            <TextInput 
+               style={styles.serchInput}
+               placeholder="Buscar devs por techs..."
+               placeholderTextColor="#999"
+               autoCapitalize="words"
+               autoCorrect={false}
+            />
+            <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+               <MaterialIcons name="my-location" size={20} color="#FFF"/>
+            </TouchableOpacity>
+         </View>
+      </>
    );
 }
 
@@ -82,5 +97,47 @@ const styles = StyleSheet.create({
    devTechs: {
       marginTop: 5,
    },
+
+   serchForm: {
+      position: 'absolute',
+      top: 20,
+      left: 20,
+      right: 20,
+      zIndex: 5,
+      flexDirection: 'row',
+   },
+
+   serchInput: {
+      flex: 1,
+      height: 50,
+      backgroundColor: '#FFF',
+      color: '#333',
+      borderRadius: 25,
+      paddingHorizontal: 20,
+      fontSize: 16,
+      shadowColor: '#000',
+      shadowOpacity: 0.2,
+      shadowOffset: {
+         width: 4,
+         height: 4,
+      },
+      elevation: 3,
+   },
+
+   loadButton: {
+      width: 50,
+      height: 50,
+      backgroundColor: '#8E4DFF',
+      borderRadius: 25,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginLeft: 15,
+   },
+
+
+
+
+
+
 });
 export default Main;
